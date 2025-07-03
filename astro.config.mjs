a2909870542732b759f8ef38de88b98b7faac719
Thomas Bonnet <thomasbnt@protonmail.com>
2025-07-03 14:12:25 +0200

    Fix Loading Issue by Simplifying Configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import lunaria from '@lunariajs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
@@ -47,9 +46,6 @@ export default defineConfig({
 					autogenerate: { directory: 'reference' },
 				},
 			],
-			plugins: [lunaria({
-				route: "/i18n",
-			})],
 		}),
 	],
 });
